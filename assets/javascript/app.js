0cbf9f2dfa417ec75434042f8159274127fad3b3
MMPark <parkmee@gmail.com>
2018-10-18 21:24:30 -0400

    single page format with mp4 background

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,53 +4,62 @@ $(document).ready(function () {
     let projects = [
         {
             title: "Psychic Guessing Game",
-            description: "Try to guess a random letter before your guesses run out",
+            description: "Try to guess a random letter before your guesses run out. The most fun you will ever have in your life.",
             imgURL: "wk3-psychic-game.PNG",
             gameURL: "https://parkmee.github.io/Psychic-Game/",
+            githubURL: "https://github.com/parkmee/Psychic-Game",
         },
         {
             title: "Safari Hangman",
             description: "Take a ride to the safari! Guess the letters of the word to spell the name of a safari animal",
             imgURL: "wk3-hangman.PNG",
             gameURL: "https://parkmee.github.io/Word-Guess-Game",
+            githubURL: "https://github.com/parkmee/Word-Guess-Game",
         },
         {
             title: "Crystal Collector",
             description: "Collect colorful crystals. If the sum of your crystals equals the target number, you win!",
             imgURL: "wk4-crystal-collector.PNG",
             gameURL: "https://parkmee.github.io/unit-4-game",
+            githubURL: "https://github.com/parkmee/unit-4-game",
         },
         {
             title: "Cat Fight!",
             description: "Select your cat and defeat all the other alley cats for the prize...a can of half eaten tuna. Yum!",
             imgURL: "wk4-cat-fight.PNG",
             gameURL: "https://parkmee.github.io/cat-fight",
+            githubURL: "https://github.com/parkmee/cat-fight",
         },
         {
             title: "World Trivia",
             description: "Test your knowledge of world geography and culture in a race against the timer.",
             imgURL: "wk5-world-trivia.PNG",
             gameURL: "https://parkmee.github.io/TriviaGame",
+            githubURL: "https://github.com/parkmee/TriviaGame",
         },
         {
             title: "World Trivia",
             description: "Giphy edition! Test your knowledge of world geography and culture in a race against the timer.",
             imgURL: "wk5-world-trivia-v2.PNG",
             gameURL: "https://parkmee.github.io/TriviaGame-v2",
+            githubURL: "https://github.com/parkmee/TriviaGame-v2",
         },
         {
             title: "Video Game GIFs",
             description: "Add and search for gifs of your favorite video games. CLick on the images to animate them.",
             imgURL: "wk-6-video-game-gifs.PNG",
             gameURL: "https://parkmee.github.io/giphy-search-tool",
+            githubURL: "https://github.com/parkmee/giphy-search-tool",
         },
         {
             title: "Train Schedule",
             description: "See a schedule of trains and see their nearest time of arrival and minutes left to arrival.",
             imgURL: "wk-7-train-schedule.PNG",
             gameURL: "https://parkmee.github.io/train-schedule/",
+            githubURL: "https://github.com/parkmee/train-schedule",
         },
     ]
+
 
     displayPortfolio();
 
@@ -60,15 +69,22 @@ $(document).ready(function () {
             
             $(".portfolio-view").prepend(
                 `
-                    <div class="project-item d-inline-block text-center border border-dark rounded">
-                        <div class="img-box">
+                    <div class="project-item d-inline-block text-center border border-dark rounded w-25">
+                        <div class="img-box w-95">
                             <a href="${projects[j].gameURL}"" target="_blank">
                                 <img class="game-img rounded" src="assets/images/${projects[j].imgURL}">
                             </a>
                         </div>
                         <div>
-                            <h2 class="project-title">${projects[j].title}</h2>
+                            <h5 class="project-title">${projects[j].title}</h5>
                             <p class="project-description">${projects[j].description}</p>
+                        </div>
+                        <div class="row">
+                            <div class="col text-center">
+                            <a href="https://github.com/parkmee" target="_blank"><i class="project-link fab fa-github-alt"></i></a>
+                            <a href="${projects[j].gameURL}"" target="_blank">
+                            <a href="portfolio.html"><i class="project-link fas fa-image"></i></a>
+                            </div>
                         </div>
                     </row>
                 `
